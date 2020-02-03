d5afb6313dd9406ecec7c24dd2b87e242ade8b8f
Student <example@example.com>
2020-02-03 11:40:35 -0800

    add basic google maps api functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Place.create!([
+  { "name": "P-Town", "lat": "45.5051", "lon": "-122.6750"},
+  { "name": "Buckingham Palace", "lat": "51.501564","lon": "-0.141944"},
+  { "name": "Westminster Abbey", "lat": "51.499581", "lon": "-0.127309"},
+  { "name": "Big Ben", "lat": "51.500792", "lon": "-0.124613"}
+  ])
