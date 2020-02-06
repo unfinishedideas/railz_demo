bf3222aa3d087b2bda3efff3b3b7562c9904da04
Student <example@example.com>
2020-02-06 12:02:32 -0800

    fixes up master

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,51 +25,50 @@ admin = User.create!(
 
   10.times do |index|
     user = User.create!(
-    :user_name => "#{unique}_" + Faker::Superhero.name,
-    :email => Faker::Lorem.characters(number: 10, min_alpha: 4) + "@user.com")
-    unique += 1
-    user_array.push(user)
-  end
+      :user_name => "#{unique}_" + Faker::Superhero.name,
+      :email => Faker::Lorem.characters(number: 10, min_alpha: 4) + "@user.com")
+      unique += 1
+      user_array.push(user)
+    end
 
-  p "Created #{User.count} users"
+    p "Created #{User.count} users"
 
-14.times do |index|
-  spots = []
-  spots.push(Spot.create!( name: Faker::Verb.unique.base.capitalize() + " Park",
-    lat: 45.5+(rand(9999)/100000),
-    lon: 122.6+(rand(9999)/100000),
-    description: Faker::Space.distance_measurement + " from " +     Faker::Verb.unique.base.capitalize() + " Park",
-    features: Faker::Construction.material + " " +   Faker::Appliance.equipment,
-    spot_type: Faker::Movies::HitchhikersGuideToTheGalaxy.starship
+    14.times do |index|
+      spots = []
+      spots.push(Spot.create!(name: Faker::Verb.unique.base.capitalize() + " Park",
+      lat: 45.5+(rand(9999)/100000),
+      lon: 122.6+(rand(9999)/100000),
+      description: Faker::Space.distance_measurement + " from " +     Faker::Verb.unique.base.capitalize() + " Park",
+      features: Faker::Construction.material + " " +   Faker::Appliance.equipment,
+      spot_type: Faker::Movies::HitchhikersGuideToTheGalaxy.starship
       ))
-  spots
-  Review.create!(
-    :title => Faker::Superhero.name,
-    :content => Faker::Movies::Lebowski.quote,
-    :user_id => user_array.sample.id,
-    :rating => rand(1..5),
-    :heat_lvl => rand(1..5),
-    :spot_id => spots.sample.id
-  )
-end
-
-  Spot.create! do ( name: "Episk8us",
-    lat: 45.520818,
-    lon: -122.677441,
-    description: "8th floor, indoor skate spot",
-    features: "Couches, waxed desks, Nate and team",
-    spot_type: "indoor skatepark"
-      )
+      spots
       Review.create!(
-        :title => "Its a School",
-        :content => "I cam here all the way fomr Cali to skate here, but its just a School, WTF? this app sucks",
+        :title => Faker::Superhero.name,
+        :content => Faker::Movies::Lebowski.quote,
         :user_id => user_array.sample.id,
-        :rating => rand(2),
-        :heat_lvl => rand(1),
+        :rating => rand(1..5),
+        :heat_lvl => rand(1..5),
         :spot_id => spots.sample.id
       )
-end
+    end
 
-p "Created #{Spot.count} spots"
-p "Created #{Review.count} reviews"
+    Spot.create!(name: "Episk8us",
+      lat: 45.520818,
+      lon: -122.677441,
+      description: "8th floor, indoor skate spot",
+      features: "Couches, waxed desks, Nate and team",
+      spot_type: "indoor skatepark"
+    )
+    Review.create!(
+      :title => "Its a School",
+      :content => "I cam here all the way fomr Cali to skate here, but its just a School, WTF? this app sucks",
+      :user_id => user_array.sample.id,
+      :rating => 2,
+      :heat_lvl => 1,
+      :spot_id => spots.sample.id
+    )
 
+
+    p "Created #{Spot.count} spots"
+    p "Created #{Review.count} reviews"
